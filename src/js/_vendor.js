e85dc942ce19e7d91afaf90866e260f1b3608886
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-04-19 00:17:41 +0300

    init

---
+++ src/js/_vendor.js
@@ -0,0 +1,46 @@
+// ========================================================================================
+
+
+// Плагин кастом-скролла
+// import 'simplebar';
+
+
+// ========================================================================================
+
+
+// Анимации по скроллу
+// import AOS from 'aos';
+// AOS.init();
+
+
+// ========================================================================================
+
+
+// Галерея
+
+// Документация: https://www.lightgalleryjs.com/docs/
+// Сниппет(HTML): gallery
+
+// Подключение базового набора функционала
+// import lightGallery from 'lightgallery';
+
+// Плагины
+// lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, 
+// lgShare, lgThumbnail, lgVideo, lgMediumZoom
+// import lgFullscreen from 'lightgallery/plugins/fullscreen/lg-fullscreen.min.js';
+
+// Запуск
+// const galleries = document.querySelectorAll('#galleryID');
+// galleries.forEach(gallery => {
+//     lightGallery(gallery, {
+//         // plugins: [lgFullscreen],
+//         licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
+//         speed: 500,
+//     });
+// });
+
+
+// ========================================================================================
+
+
+
